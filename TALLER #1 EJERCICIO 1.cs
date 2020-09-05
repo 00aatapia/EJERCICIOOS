9fb50a968c68d46c8b7158ad07457484ec39d67f
00aatapia <70782851+00aatapia@users.noreply.github.com>
2020-09-04 23:23:49 -0500

    Add files via upload

---
+++ TALLER #1 EJERCICIO 1.cs
@@ -0,0 +1,40 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+
+
+   float Nota_1, Nota_2, Nota_3, Promedio;
+
+
+            Console.WriteLine("Digite la nota 1:");
+            Nota_1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la nota 2:");
+            Nota_2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la nota 3:");
+            Nota_3 = float.Parse(Console.ReadLine());
+
+            Promedio = (Nota_1 + Nota_2 + Nota_3) / 3;
+
+
+            Console.WriteLine("El promedio es :" + Promedio);
+
+            if (Promedio >= 3)
+            {
+
+                Console.WriteLine("El estudiante aprobo la asignatura");
+
+
+            }
+            else
+            {
+                Console.WriteLine("El estudiante reprobo la asignatura");
+
+            }
+  
+  
+  
+  
+  }
+}
---
+++ TALLER #1 EJERCICIO 2.cs
@@ -0,0 +1,36 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    String Genero;
+            float Edad;
+            float Pulsaciones;
+          
+            Console.WriteLine("DIGITE EN MAYUSCULA MASCULINO/FEMENINO (M/F)");
+            Genero = Console.ReadLine();
+
+            if ((Genero == "M") || (Genero =="m"))
+            {
+                Console.WriteLine("SU GENERO ES ---> MASCULINO  ");
+                Console.WriteLine("DIGITE SU EDAD : ");
+                Edad = float.Parse(Console.ReadLine());
+                Pulsaciones = ((210 - Edad)) / 10;
+                Console.WriteLine("SU NUMERO DE PULSACIONES ES DE : " + Pulsaciones);
+
+            }
+
+
+            else if ((Genero == "F") || (Genero =="f"))
+            {
+                Console.WriteLine("SU GENERO ES ---> FEMENINO  ");
+                Console.WriteLine("DIGITE SU EDAD : ");
+                Edad = float.Parse(Console.ReadLine());
+                Pulsaciones = ((220 - Edad)) / 10;
+                Console.WriteLine("SU NUMERO DE PULSACIONES ES DE : " + Pulsaciones);
+
+            }
+
+
+  }
+}
